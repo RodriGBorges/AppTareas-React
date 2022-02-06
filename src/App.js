b2e616b5ad0e190c3765a7eea9bfecdb44b42e8b
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-06 18:54:12 -0300

    Componente listado de tareas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from 'react';
 import { tareasReducer } from './Reducers/tareasReducer';
 import { AgregarTarea } from './Components/AgregarTarea';
 import './App.css';
+import { ListadoTareas } from './Components/ListadoTareas';
 
 
 /* const initalState = [
@@ -78,21 +79,12 @@ function App() {
       <hr/>
       <div className='row'>
         <div className='col-7'>
-          <ul className='list-group list-group-flush px-4'>
-            {
-              tareas.map( ({descripcion, terminada, id}, i) => (
-                //map i = indice, recorrido del map 0,1,2,3,4...
-                <li className='d-flex justify-content-between align-item-center' key={descripcion + i}>
-                  <p onClick={ () => handleUpdate(id)} className={terminada && 'text-decoration-line-through'}>
-                    {i + 1}.{descripcion}
-                  </p>
-                  <button className='btn btn-sm btn-danger mb-1' onClick={ () => handleDelete(id)}>
-                    <i className='fas fa-trash-alt'></i>
-                  </button>
-                </li>
-              ))
-            }
-          </ul>
+          <ListadoTareas
+          //pasamos por props las tareas, y los métodos de borrar tarea y actualizar tarea
+            tareas = {tareas}
+            handleDelete = {handleDelete}
+            handleUpdate = {handleUpdate}
+          />
         </div>
         <div className='col-5'>
           <AgregarTarea
--- src/Components/ListadoTareas.js
+++ src/Components/ListadoTareas.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-export const ListadoTareas = () => {
-  return <div></div>;
+export const ListadoTareas = ({tareas, handleDelete, handleUpdate}) => {
+
+  return (
+
+    <ul className='list-group list-group-flush px-4'>
+      {
+        tareas.map( ({descripcion, terminada, id}, i) => (
+          //map i = indice, recorrido del map 0,1,2,3,4...
+          <li className='d-flex justify-content-between align-item-center' key={descripcion + i}>
+            <p onClick={ () => handleUpdate(id)} className={terminada && 'text-decoration-line-through'}>
+              {i + 1}.{descripcion}
+            </p>
+            <button className='btn btn-sm btn-danger mb-1' onClick={ () => handleDelete(id)}>
+              <i className='fas fa-trash-alt'></i>
+            </button>
+          </li>
+        ))
+      }
+    </ul>
+
+  );
+
 };
